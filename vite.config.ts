991380a37a3dee88fcefa670f4418828928054ec
HaithB <haithembuiseness@gmail.com>
2025-11-09 23:10:25 +0000

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  // Base needs to be relative for standard GitHub Pages deployment unless using a custom domain
+  base: './',
+})
